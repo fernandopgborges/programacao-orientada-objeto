620c918288749bfb38ee084c023849bad5a2e7de
Fernando <fernandopgb02@gmail.com>
2026-03-09 21:42:23 -0300

    Testando

---
+++ Março/2026-03-09/Conta.java
@@ -0,0 +1,8 @@
+public class Conta {
+    private double saldo;
+    private double limite;
+
+    // Tudo que é static é apenas da Classe e não do Objeto
+    private static int qtdContas = 2; // Roda apenas uma vez
+    public static final double TAXA = 3.5; // Roda apenas uma vez e é uma constante
+}
